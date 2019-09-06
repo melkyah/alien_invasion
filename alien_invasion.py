5cf192e7851d804dc00415fe6e12eab8da0beebb
Francisco Gonzalez Ratti <franciscog.mk@gmail.com>
2019-09-06 17:33:05 -0300

    Added ship class and instance

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,26 @@
+"""
+This module implements the main components of the game.
+"""
+
 import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
+    """
+    This function runs the game.
+    """
     # Initialize pygame, settings, and screen object
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship.
+    ship = Ship(screen)
 
     # Start the main loop for the game.
     while True:
@@ -21,6 +32,7 @@ def run_game():
 
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
