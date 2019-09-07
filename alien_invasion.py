3dbfe399ef227d28254b910b1ab901940cba5e86
fgonzalez@gcpsandbox.cloud <gonzalezratti@google.com>
2019-09-07 12:56:31 -0300

    Added movement to the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,6 +31,7 @@ class AlienInvasion(object):
         # Start the main loop for the game.
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -38,6 +39,16 @@ class AlienInvasion(object):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
@@ -50,4 +61,3 @@ if __name__ == '__main__':
     #Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
-    
