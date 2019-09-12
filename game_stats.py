b93748aa20fe519c9b8f9e59d5876eb9ebdd2c01
Francisco Gonzalez <franciscog.mk@gmail.com>
2019-09-11 22:19:54 -0300

    Added Play button

--- game_stats.py
+++ game_stats.py
@@ -10,8 +10,9 @@ class GameStats(object):
         """Initialize statistics"""
         self.settings = ai_game.settings
         self.reset_stats()
-        # Start Alien Invasion in an active state.
-        self.game_active = True
+
+        # Start game in an inactive state.
+        self.game_active = False
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
